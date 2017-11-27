05fc11d984cedf822e763ebb195f58460ab20d27
Nathan Schwartz <nathan.schwartz95@gmail.com>
2017-11-27 11:50:55 -0600

    refactor(test): Refactor and add query tests
    Splits the query tests out of schemaGeneration into a new file.
    
    Refactor query test suite such that it can be easily re-run with a
    different query resolution strategy.
    
    Also changes test imports from src to package.json's "main"
    file. This necessitates rebuilding the lib folder before
    running tests, but the benefit is a more thorough test suite.

--- tests/schemaGeneration.spec.js
+++ tests/schemaGeneration.spec.js
@@ -1,123 +1,11 @@
 import { ServiceBroker, Transporters } from 'moleculer';
-import { graphql as execute, printSchema } from 'graphql';
-import { GraphQLGateway } from '../src/Gateway/GraphQLGateway';
+import { printSchema } from 'graphql';
+import { GraphQLGateway } from '..';
 import authorSvc from './types/Author';
 import bookSvc from './types/Book';
 import chapterSvc from './types/chapter';
-import * as dataSource from './types/data';
 
 jest.setTimeout(10000);
-
-async function authorsQuery(schema) {
-  const { data } = await execute(schema, `{
-    authors {
-      name,
-      id,
-    }
-  }`);
-  expect(data).toEqual({ authors: dataSource.authors });
-}
-
-async function authorsWithBooksQuery(schema) {
-  const { data } = await execute(schema, `{
-    authors {
-      name,
-      id,
-      books {
-        title,
-        year,
-        authorId,
-        id
-      }
-    }
-  }`);
-  expect(data).toEqual({
-    authors: dataSource.authors.map(author => ({
-      ...author,
-      books: dataSource.books.filter(book => book.authorId === author.id),
-    })),
-  });
-}
-
-async function authorsWithBooksAndChaptersQuery(schema) {
-  const { data } = await execute(schema, `{
-    authors {
-      name,
-      id,
-      books {
-        title,
-        year,
-        authorId,
-        id
-        chapters {
-          title,
-          id,
-          bookId,
-        }
-      }
-    }
-  }`);
-  expect(data).toEqual({
-    authors: dataSource.authors.map(author => ({
-      ...author,
-      books: dataSource.books.filter(book => book.authorId === author.id).map(book => ({
-        ...book,
-        chapters: dataSource.chapters.filter(chapter => chapter.bookId === book.id)
-      }))
-    })),
-  });
-}
-
-async function booksQuery(schema) {
-  const { data } = await execute(schema, `{
-    books {
-      title,
-      year,
-      id,
-      authorId
-    }
-  }`);
-  expect(data).toEqual({ books: dataSource.books });
-}
-
-async function booksWithAuthorQuery(schema) {
-  const { data } = await execute(schema, `{
-    books {
-      title,
-      year,
-      id,
-      authorId
-      author {
-        name
-        id
-      }
-    }
-  }`);
-  expect(data).toEqual({ books: dataSource.books.map(book => ({
-    ...book,
-    author: dataSource.authors.find(author => author.id === book.authorId)
-  })) });
-}
-
-async function booksWithChaptersQuery(schema) {
-  const { data } = await execute(schema, `{
-    books {
-      title,
-      year,
-      id,
-      authorId
-      chapters {
-        bookId
-        title
-        id
-      }
-    }
-  }`);
-  expect(data).toEqual({ books: dataSource.books.map(book => ({
-    ...book,
-    chapters: dataSource.chapters.filter(chapter => chapter.bookId === book.id)
-  })) });
-}
 
 describe('Schema Generation', () => {
   describe('With A Single Broker', () => {
@@ -144,30 +32,6 @@ describe('Schema Generation', () => {
     });
 
     afterAll(() => broker.stop());
-
-    test('Should be able to query for all authors', async () => {
-      return await authorsQuery(gateway.schema);
-    });
-
-    test('Should be able to include books on author query', async () => {
-      return await authorsWithBooksQuery(gateway.schema);
-    });
-
-    test('Should be able to include chapters inside books on author query', async () => {
-      await authorsWithBooksAndChaptersQuery(gateway.schema);
-    });
-
-    test('Should be able to query for all books', async () => {
-      await booksQuery(gateway.schema);
-    });
-
-    test('Should be able to include the author in the books query', async () => {
-      await booksWithAuthorQuery(gateway.schema);
-    });
-
-    test('Should be able to include the chapters in the books query', async () => {
-      await booksWithChaptersQuery(gateway.schema);
-    });
 
     test('Should generate a consistant schema', () => {
       expect(printSchema(gateway.schema)).toMatchSnapshot();
@@ -226,30 +90,6 @@ describe('Schema Generation', () => {
       bookBroker.stop();
       chapterBroker.stop();
     })
-
-    test('Should be able to query for all authors', async () => {
-      return await authorsQuery(gateway.schema);
-    });
-
-    test('Should be able to include books on author query', async () => {
-      return await authorsWithBooksQuery(gateway.schema);
-    });
-
-    test('Should be able to include chapters inside books on author query', async () => {
-      await authorsWithBooksAndChaptersQuery(gateway.schema);
-    });
-
-    test('Should be able to query for all books', async () => {
-      await booksQuery(gateway.schema);
-    });
-
-    test('Should be able to include the author in the books query', async () => {
-      await booksWithAuthorQuery(gateway.schema);
-    });
-
-    test('Should be able to include the chapters in the books query', async () => {
-      await booksWithChaptersQuery(gateway.schema);
-    });
 
     test('Should generate a consistant schema', () => {
       expect(printSchema(gateway.schema)).toMatchSnapshot();
